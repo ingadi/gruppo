8500bb84a46f8fd7f016d2cf4901818e472bd3da
ingadi <axelingadi@gmail.com>
2022-10-23 14:30:56 +0300

    add a toast component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from "./features/Header";
 import Content from "./features/Content";
 import "./App.css";
 import Modal from "./components/Modal";
+import Toast from "./components/Toast";
 
 function App() {
   return (
     <main>
       {/* <Modal /> */}
+      <Toast />
       <Header />
       <Content />
     </main>
---
+++ src/components/Toast.jsx
@@ -0,0 +1,11 @@
+import styles from "./Toast.module.css";
+
+function Toast() {
+  return (
+    <div className={styles.toast}>
+      <p>This is a toast</p>
+    </div>
+  );
+}
+
+export default Toast;
