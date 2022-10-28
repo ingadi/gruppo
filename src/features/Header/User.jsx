6058f58f8d52308576db6adf73409e6ea6b466d4
ingadi <axelingadi@gmail.com>
2022-10-28 18:24:27 +0300

    update avatar ui

--- src/features/Header/User.jsx
+++ src/features/Header/User.jsx
@@ -1,10 +1,10 @@
 import { logOut } from "../../api/auth";
 import { AiOutlineUser } from "react-icons/ai";
 import { FiLogOut } from "react-icons/fi";
-import userStyles from "./User.module.css";
-import buttonStyles from "../../components/Button.module.css";
+import styles from "./User.module.css";
+// import buttonStyles from "../../components/Button.module.css";
 
-const styles = { ...userStyles, ...buttonStyles };
+// const styles = { ...userStyles, ...buttonStyles };
 
 function User() {
   const handleLogout = async () => {
@@ -17,23 +17,38 @@ function User() {
   };
 
   return (
-    <div tabIndex="0" className={styles.user}>
-      {/*todo use css modules compose to reduce*/}
-      <span
-        className={`${styles.avatar} ${styles.btn} ${styles.md} ${styles["secondary-outline"]} ${styles.rounded}`}
-      >
-        <AiOutlineUser className={styles["icon-md"]} />
-        Tim Apple
-      </span>
-      <ul className={styles.actions}>
-        <li>
-          <a onClick={handleLogout}>
+    <div className={styles.user}>
+      <label tabIndex={0} htmlFor="userAvatar">
+        {/* <input type="checkbox" name="" id="userAvatar" /> */}
+        <div className={styles.avatar}>
+          <img src="" alt="" className={styles["user-img"]} />
+          <span className={styles.username}> Tim Apple</span>
+        </div>
+        <ul>
+          <li onClick={handleLogout}>
             <FiLogOut />
             Logout
-          </a>
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </label>
     </div>
+    // <div tabIndex="0" className={styles.user}>
+    //   {/*todo use css modules compose to reduce*/}
+    //   <span
+    //     className={`${styles.avatar} ${styles.btn} ${styles.md} ${styles["secondary-outline"]} ${styles.rounded}`}
+    //   >
+    //     <AiOutlineUser className={styles["icon-md"]} />
+    //     Tim Apple
+    //   </span>
+    //   <ul className={styles.actions}>
+    //     <li>
+    //       <a onClick={handleLogout}>
+    //         <FiLogOut />
+    //         Logout
+    //       </a>
+    //     </li>
+    //   </ul>
+    // </div>
   );
 }
 
