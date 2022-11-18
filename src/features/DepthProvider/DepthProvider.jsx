1b0f3110765547738077aab1ae6541da5a522d00
ingadi <axelingadi@gmail.com>
2022-11-18 22:22:09 +0300

    persist depth in local storage

--- src/features/DepthProvider/DepthProvider.jsx
+++ src/features/DepthProvider/DepthProvider.jsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import { createContext } from "react";
 
 const DepthContext = createContext();
 
 export function DepthProvider({ children }) {
-  const [depth, setDepth] = useState([]);
+  const [depth, setDepth] = useState(
+    JSON.parse(localStorage.getItem("depth")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("depth", JSON.stringify(depth));
+  }, [depth]);
+
   return (
     <DepthContext.Provider value={{ depth, setDepth }}>
       {children}
